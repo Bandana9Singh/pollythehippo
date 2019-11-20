534d0664847fef2ad0466a5c2fe3b83d92f36781
Bandana9Singh <bandana800@gmail.com>
2019-11-20 16:45:14 +0530

    Initial Commit to Master Branch

---
+++ site/components/src/main/java/org/project/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.project.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="pollythehippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
